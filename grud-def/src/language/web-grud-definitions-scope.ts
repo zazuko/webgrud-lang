757c98bcdb5a180e8781df794665b4c47b3abfbb
Michael Rauch <git.mra@miranet.ch>
2024-08-05 16:05:00 +0200

    grammar: include calculations and conditions

--- grud-def/src/language/web-grud-definitions-scope.ts
+++ grud-def/src/language/web-grud-definitions-scope.ts
@@ -1,6 +1,7 @@
 import type { WebGrudDefinitionsServices } from './web-grud-definitions-module.js';
 import type { AstNode, AstNodeDescription, LangiumDocument, PrecomputedScopes } from 'langium';
-import type { Model, Factor } from './generated/ast.js';
+import type { Model, Cube } from './generated/ast.js';
+import { isSourcedValue } from './generated/ast.js';
 import { Cancellation, DefaultScopeComputation, interruptAndCheck, MultiMap } from 'langium';
 
 export class WebGrudDefinitionsScopeComputation extends DefaultScopeComputation {
@@ -12,24 +13,54 @@ export class WebGrudDefinitionsScopeComputation extends DefaultScopeComputation 
     override async computeLocalScopes(document: LangiumDocument, cancelToken = Cancellation.CancellationToken.None): Promise<PrecomputedScopes> {
         const model = document.parseResult.value as Model;
         const scopes = new MultiMap<AstNode, AstNodeDescription>();
+        const cube2DimensionDescriptionsDict = new MultiMap<Cube, AstNodeDescription>();
 
         for (const factor of model.factors) {
             await interruptAndCheck(cancelToken);
-            await this.includeDimensionsInScope(factor, scopes, document, cancelToken);
+
+            if (factor.cube?.ref) {
+                const cube = factor.cube.ref;
+                let descriptions;
+                if (cube2DimensionDescriptionsDict.has(cube)) {
+                    descriptions = cube2DimensionDescriptionsDict.get(cube);
+                } else {
+                    descriptions = await this.buildDescriptionsForDimensions(cube, document, cancelToken);
+                    cube2DimensionDescriptionsDict.addAll(cube, descriptions);
+                }
+                
+                scopes.addAll(factor, descriptions);
+            }
+        }
+
+        for (const sourcedValue of model.values.filter(isSourcedValue)) {
+            await interruptAndCheck(cancelToken);
+
+            if (sourcedValue.cube?.ref) {
+                const cube = sourcedValue.cube.ref;
+                let descriptions;
+                if (cube2DimensionDescriptionsDict.has(cube)) {
+                    descriptions = cube2DimensionDescriptionsDict.get(cube);
+                } else {
+                    descriptions = await this.buildDescriptionsForDimensions(cube, document, cancelToken);
+                    cube2DimensionDescriptionsDict.addAll(cube, descriptions);
+                }
+                
+                scopes.addAll(sourcedValue, descriptions);
+            }
         }
         
         return scopes;
     }
 
-    protected async includeDimensionsInScope(factor: Factor, scopes: PrecomputedScopes, document: LangiumDocument, cancelToken: Cancellation.CancellationToken): Promise<AstNodeDescription[]> {
+    protected async buildDescriptionsForDimensions(cube: Cube, document: LangiumDocument, cancelToken: Cancellation.CancellationToken): Promise<AstNodeDescription[]> {
         const localDescriptions: AstNodeDescription[] = [];
-        if (factor.cube?.ref) {            
-            for (const dimension of factor.cube.ref.dimensions) {
+        if (cube) {
+            for (const dimension of cube.dimensions) {
                 await interruptAndCheck(cancelToken);
+
                 const description = this.descriptions.createDescription(dimension, dimension.name, document);
                 localDescriptions.push(description);
             }
-            scopes.addAll(factor, localDescriptions);            
         }
 
         return localDescriptions;
